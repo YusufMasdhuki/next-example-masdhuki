a38f5e95fba4da62b5851bacec66601dfeeca0d4
YusufMasdhuki <yusufmasduki19@gmail.com>
2025-06-22 14:01:35 +0700

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <>
       <h1>hello world</h1>
+      <p>
+        ini adalah web <span className='font-bold'>YUSUF MASDHUKI</span>
+      </p>
       <Image
         src='https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
         alt=''
