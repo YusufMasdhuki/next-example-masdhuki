8e5973d0ce1d2559f7964cc2f16dff98227aaa91
YusufMasdhuki <yusufmasduki19@gmail.com>
2025-06-22 10:19:45 +0700

    init commit

---
+++ src/pages/post-server/[id].tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  const paths = data.map((post: any) => {
+    return {
+      params: {
+        id: post.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const data = await res.json();
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
+const PostView = ({ post }: any) => {
+  return (
+    <div>
+      <h1 className='text-3xl'>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+export default PostView;
